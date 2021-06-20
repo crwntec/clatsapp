ad40c63fa5e0a1041665aece806f85444068a932
BuildTools <unconfigured@null.spigotmc.org>
2021-06-20 21:58:39 +0200

    Added ChatView Header(STATIC) and BG

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./Chat.css"
+import Avatar from '@material-ui/core/Avatar';
+
+import { IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 
 function Chat() {
+    const [seed, setSeed] = useState('')
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+
+    }, [])
+
     return (
-        <div>
-            
+        <div className="Chat">
+            <div className="chat_header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+                <div className="chat_headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen...</p>
+                </div>
+
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+        
+
+            <div className="chat_body">
+                
+
+            </div>
+
+            <div className="chat_footer">
+
+            </div>
         </div>
-    )
+    );
 }
 
 export default Chat
