c6ad0780eabd64722fa9534b344a288fa40c282d
BuildTools <unconfigured@null.spigotmc.org>
2021-06-20 18:59:04 +0200

    Sidebar Search/Header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { IconButton } from '@material-ui/core';
 
-function sidebar() {
+import './Sidebar.css'
+import { SearchOutlined } from '@material-ui/icons';
+
+function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <Avatar className="avatar"/>
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or Start a new Chat" type="text"/>
+                </div>
+            </div>
+
+            <div className="sidebar_Chats">
+                
+                
+            </div>
         </div>
-    )
+    );
 }
 
-export default sidebar
+export default Sidebar;
