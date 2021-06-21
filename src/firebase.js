17470b8d7a50b76b2d8a57f156f30dd6ac8203a6
BuildTools <unconfigured@null.spigotmc.org>
2021-06-21 18:43:54 +0200

    Added firebase Cfg

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPpHRGpO0fKk13NKHqqZ9UZfnZhNP5Xk0",
+    authDomain: "clatsapp.firebaseapp.com",
+    projectId: "clatsapp",
+    storageBucket: "clatsapp.appspot.com",
+    messagingSenderId: "973102943536",
+    appId: "1:973102943536:web:6f502d0427a8e533488cf5",
+    measurementId: "G-69CBSVF5QQ"
+  };
